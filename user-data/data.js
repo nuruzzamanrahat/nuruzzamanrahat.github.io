a2cd359ad053c13c6cb8f59cb259b707a3755513
Nuruzzaman Rahat <nuruzzamanrahat1738@gmail.com>
2025-10-22 16:44:24 +0600

    Initial Commit

---
+++ user-data/data.js
@@ -0,0 +1,178 @@
+const data = {
+  "bio": [
+    "Hi, I'm Nuruzzaman Rahat",
+    "Dedicated to applying mathematical expertise to real-world challenges through the field of applied mathematics, with a focus on developing models for solving practical problems across diverse domains, including science, engineering, biology, and industry. Passionate about leveraging advanced mathematical techniques, computational tools, and interdisciplinary approaches to analyze complex systems, optimize solutions, and contribute to data-driven decision-making."
+  ],
+  "skills": [
+    "Python",
+    "C++",
+    "MATLAB",
+    "Wolfram Mathematica",
+    "COMSOL Multiphysics",
+    "HTML5",
+    "SQL",
+    "Git",
+    "OCR",
+    "OpenAI APIs",
+    "RAG",
+    "LLMs",
+    "NLP",
+    "Embeddings",
+    "VectorDBs",
+    "Prompt Engineering",
+    "Microsoft Office Tools",
+    "LaTeX",
+    "Adobe Illustrator",
+    "Canva"
+  ],
+  "work_experience": [
+    {
+      "title": "ACE Advisory",
+      "duration": "August 2025 - Present",
+      "subtitle": "Data Engineer",
+      "details": [
+        "Currently developing a Retrieval-Augmented Generation (RAG) pipeline integrating LLMs with vector-based retrieval systems to assist in legal and audit document analysis.",
+        "Designing data ingestion, preprocessing, and embedding workflows for unstructured legal text.",
+        "Exploring prompt engineering and context optimization techniques to enhance model grounding and factual accuracy.",
+        "Evaluating retrieval performance and response quality through experimental testing and analytical assessment.",
+        "Collaborating with AI researchers and legal professionals to apply data-driven and research-oriented approaches in applied NLP development."
+      ],
+      "tags": [
+        "HTML5",
+        "SQL",
+        "Git",
+        "OCR",
+        "OpenAI APIs",
+        "RAG",
+        "LLMs",
+        "NLP",
+        "Embeddings",
+        "VectorDBs",
+        "Prompt Engineering"
+      ],
+      "icon": "industry"
+    },
+    {
+      "title": "Achieve (EdTech Company)",
+      "duration": "January 2024 - July 2025",
+      "subtitle": "Senior Content Executive (Mathematics)",
+      "details": [
+        "Designed and developed high-quality mathematics learning materials (lesson plans, problem sets, quizzes, and interactive exercises) aligned with national STEM curricula.",
+        "Translated complex mathematical concepts into engaging, easy-to-understand content suitable for diverse learner levels.",
+        "Guided and mentored junior content writers or editors, ensuring adherence to quality benchmarks and project timelines."
+      ],
+      "tags": [
+        "Microsoft Office Tools",
+        "LaTeX",
+        "Adobe Illustrator",
+        "Canva"
+      ],
+      "icon": "School"
+    }
+  ],
+  "education": [
+    {
+      "title": "Master of Science (MSc) in Applied Mathematics",
+      "duration": "2024 - 2025",
+      "subtitle": "University of Dhaka",
+      "details": [""],
+      "tags": [],
+      "icon": "graduation-cap"
+    },
+    {
+      "title": "Bachelor of Science (BSc) in Applied Mathematics",
+      "duration": "2019-2023",
+      "subtitle": "University of Dhaka",
+      "details": [
+        "GPA-3.60 (out of 4.00)",
+        "Project Title: Effective Numerical Method for Solving Ordinary and Partial Differential Equations"
+      ],
+      "tags": [],
+      "icon": "graduation-cap"
+    },
+    {
+      "title": "HSC | Science",
+      "duration": "2016-2018",
+      "subtitle": "Govt. Science College",
+      "details": ["GPA-4.75 (out of 5.00)"],
+      "tags": [],
+      "icon": "graduation-cap"
+    },
+    {
+      "title": "SSC | Science",
+      "duration": "2014-2016",
+      "subtitle": "Alhaz Abbas Uddin High School",
+      "details": ["GPA-5.00 (out of 5.00)"],
+      "tags": [],
+      "icon": "graduation-cap"
+    }
+  ],
+  "contactLinks": [
+    {
+      "label": "Email",
+      "link": "mailto:vinaysomawat40@gmail.com",
+      "icon": "fa fa-envelope"
+    },
+    {
+      "label": "LinkedIn",
+      "link": "https://www.linkedin.com/in/vinaysomawat/",
+      "icon": "fa-brands fa-linkedin"
+    },
+    {
+      "label": "GitHub",
+      "link": "https://github.com/vinaysomawat",
+      "icon": "fa-brands fa-github"
+    },
+    {
+      "label": "Buy me a coffee",
+      "link": "https://www.buymeacoffee.com/r194dME8y",
+      "icon": "fa fa-coffee"
+    }
+  ],
+  "footer": [
+    {
+      "label": "Links",
+      "data": [
+        {
+          "text": "Buy me a coffee",
+          "link": "https://www.buymeacoffee.com/r194dME8y"
+        },
+        {
+          "text": "Github",
+          "link": "https://github.com/vinaysomawat/vinaysomawat.github.io"
+        }
+      ]
+    },
+    {
+      "label": "Pages",
+      "data": [
+        {
+          "text": "Trekking & Himalayan Pass Adventures",
+          "link": "./pages/adventure.html"
+        },
+        {
+          "text": "Generate JSON data",
+          "link": "./pages/json-generator.html"
+        }
+      ]
+    },
+    {
+      "label": "copyright-text",
+      "data": [
+        "Built with HTML, CSS, JavaScript, and ❤️"
+      ]
+    }
+  ]
+}
+
+export const bio = data.bio;
+
+export const skills = data.skills;
+
+export const experience = data.experience
+
+export const education = data.education
+
+export const contactLinks = data.contactLinks
+
+export const footer = data.footer;
