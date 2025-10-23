cc07a53a9c0e96a4f30f42d1413d61c9ae5777c4
Nuruzzaman Rahat <nuruzzamanrahat1738@gmail.com>
2025-10-23 13:47:17 +0600

    update urls.js

--- user-data/urls.js
+++ user-data/urls.js
@@ -1,9 +1,13 @@
 const githubUsername = "nuruzzamanrahat";
+const gitConnectedUsername = "nuruzzamanrahat";
+const mediumUsername = "";
 
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
 const gitRepos = (username) => `https://pinned.berrysauce.dev/get/${username}`;
+const mediumRSS = (username) => username ? `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}` : "";
 
 export const URLs = {
-    gitConnected: createGitConnectedURL(githubUsername),
+    gitConnected: createGitConnectedURL(gitConnectedUsername),
     gitRepo: gitRepos(githubUsername),
+    medium: mediumRSS(mediumUsername),
 };
