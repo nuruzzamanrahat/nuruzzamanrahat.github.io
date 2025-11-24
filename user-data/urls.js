10db919d9ade19e7c27b23f29081a3ee804f1e5b
Nuruzzaman Rahat <nuruzzamanrahat1738@gmail.com>
2025-11-24 18:05:26 +0600

    Update LinkedIn image URL to higher resolution

--- user-data/urls.js
+++ user-data/urls.js
@@ -3,7 +3,7 @@ const gitConnectedUsername = "nuruzzamanrahat1738";
 const mediumUsername = "";
 
 // Put your LinkedIn profile image URL here
-const linkedInImageURL = "https://media.licdn.com/dms/image/v2/D4E03AQE7VPf_jLkmmw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1699814740368?e=1762992000&v=beta&t=E2x5MqhBIdhzYaxd3xv0ipbZwY_zCYfA8VUDs8X3sLA";
+const linkedInImageURL = "https://media.licdn.com/dms/image/v2/D4E03AQE7VPf_jLkmmw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1699814740368?e=1765411200&v=beta&t=-gtb3Ts0iC2KCIXSBzLiL_cK88VtJJgSSV7EEP-MUek";
 
 const createGitConnectedURL = (username) => `https://gitconnected.com/v1/portfolio/${username}`;
 const gitRepos = (username) => `https://pinned.berrysauce.dev/get/${username}`;
